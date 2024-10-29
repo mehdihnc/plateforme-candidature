d0604272090cf8c150f8c26fb2e2cc5a2bd53011
Enzo Makked <zakuu@air-de-enzo.home>
2024-10-29 20:01:38 +0100

    settings fixed

--- utilisateurs/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile
-
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'user_type', 'cv')
-    search_fields = ('user__username', 'user_type')
-    list_filter = ('user_type',)
-
-admin.site.register(UserProfile, UserProfileAdmin)
